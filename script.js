a2985e4d951b741c1ca9d9de72dcfabc2f88ff64
lauraspaghetti <laura.stassi17@gmail.com>
2020-07-30 23:01:26 +0200

    :art:  script update
    The user's entry is conveted into an array. It is now possible to compare the user's entry array to the array of comedogenic ingredients we have, and see if there are some in the entry

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ console.log('script.js');
 retrieve = () =>{
     const compoEntry=    document.getElementById('compo').value;
     // console.log(compoEntry);  
-    return compoEntry; 
+    return compoEntry.toLowerCase(); 
 }; 
 
 ///Then let's apply a function to the form, preventing it to send the results to the server
 //First we need to aim the form
-const productForm=  document.getElementById('product-form'); 
+const productForm=      document.getElementById('product-form'); 
 console.log(productForm); 
 //Next we set an event to the form, calling the function anytime the user submit the form
 productForm.addEventListener("submit", onSubmit); 
@@ -22,6 +22,28 @@ function onSubmit(event){
     //retrieving the user entry
     let compo= retrieve(); 
     console.log(compo); 
+    
+    //converting the user's entry into an array
+    stringToArray= (composition) => composition.split(/[,.]/g).map(item => item.trim());
+    let compoArray= stringToArray(compo); 
+    console.log(compoArray); 
+
+    //comparing the user's entry with the ingredients array 
+    // console.log(ingredients); 
+    for (let i= 0; i < ingredients.length; i++){
+        console.log(`Comedogenic ingredient : ${ingredients[i][0]}`); 
+        let comedogenicIngredient= ingredients[i][0];
+
+        for (let i=0; i < compoArray.length; i++){
+            console.log(compoArray[i]); 
+
+            if (comedogenicIngredient === compoArray[i]){
+                console.log("There is a comedogenic ingredient in the user's entry"); 
+            };
+        };
+
+    };
+   
 };
 
 
