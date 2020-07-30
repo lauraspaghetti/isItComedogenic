583c7cb5df229faab36781a623b4dbb47bfa290b
lauraspaghetti <laura.stassi17@gmail.com>
2020-07-30 22:59:38 +0200

    :bento: ingredient list added

---
+++ ingredients.js
@@ -0,0 +1,20 @@
+console.log('ingredients.js'); 
+
+//Let's store all the comedogenic and irritative ingredients with their comedogenic/iritation rate in an array
+let ingredients = [
+    //The first number of each arrays stands for the comedogenic rate
+    //The second number of each arrays stands for the iritation rate
+    ["acetylated lanolin", 4, 0], 
+    ["alcohol", 0, 5], 
+    ["algae extract", 5, 0], 
+    ["algin", 4, 4],
+    ["butyl stereate", 3, 0], 
+    ["butylated hydroxyanisole", 2, 3], 
+    ["cajeput oil", 2, 4],
+    ["caprylic acid", 1, 3],
+    ["carragean", 5, 0],
+    ["cetearyl acohol + ceteareth 20", 4, 0],
+
+]; 
+
+//console.log(ingredients); 
